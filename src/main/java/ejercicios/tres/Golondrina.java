bd2e7777047ef3e88bc641724342c2e68ff72c2c
Enrique Molinari <enrique.molinari@gmail.com>
2026-05-24 16:40:47 -0300

    dos ejercicios mas

---
+++ src/main/java/ejercicios/tres/Golondrina.java
@@ -0,0 +1,21 @@
+package ejercicios.tres;
+
+public class Golondrina {
+    private int energia; //joules
+
+    public Golondrina() {
+        this.energia = 45;
+    }
+
+    public void comer() {
+        // implementar ...
+    }
+
+    public void volar() {
+        // implementar ...
+    }
+
+    public void realizarDeseo() {
+        //implementar ...
+    }
+}
